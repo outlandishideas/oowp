526a8ea0781fed6328745e4b44b1c1ba6d2857dc
Harry <harryrobbins@gmail.com>
2012-06-15 17:10:20 +0100

    Changed default item to remove div and added classes to outer a tag

--- partials/item.php
+++ partials/item.php
@@ -1,6 +1,4 @@
-<a href='<?php print $post->permalink(); ?>'>
-	<div class='item item-<?php print $post::postType(); ?>'>
+<a href='<?php print $post->permalink(); ?>'  class='item item-<?php print $post::postType(); ?>'>
 		<h3><?php print $post->title(); ?></h3>
 		<?php print $post->excerpt(); ?>
-	</div>
 </a>
