f7b6eaae7f4a5084e5de48c35c374893df27b92c
Tamlyn Rhodes <tamlyn@tamlyn.org>
2013-01-07 17:08:35 +0000

    Making slug editable in admin

--- classes/ooRoutemasterPost.class.php
+++ classes/ooRoutemasterPost.class.php
@@ -4,15 +4,19 @@ abstract class ooRoutemasterPost extends ooPost {
 
 	/**
 	 * Generates the permalink by concatenating this post's name to its parent's (recursively)
+	 * @param bool $leaveName
 	 * @return string
 	 */
-	public function permalink()
-	{
+	public function permalink($leaveName = false) {
 		/** @var $parent ooPost */
 		$parent = $this->getParent();
-		$parentUrl = $parent ? $parent->permalink() : get_bloginfo('url').'/';
+		$parentUrl = $parent ? $parent->permalink() : get_bloginfo('url') . '/';
 		$homepage = self::fetchHomepage();
-		$postName = ($homepage && $this->ID == $homepage->ID) ? '' : $this->post_name . '/';
-		return $parentUrl.$postName;
+		if ($homepage && $this->ID == $homepage->ID) {
+			return $parentUrl;
+		} else {
+			$postName = $leaveName ? '%postname%' : $this->post_name;
+			return $parentUrl . $postName . '/';
+		}
 	}
 }
