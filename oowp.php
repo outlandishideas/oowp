ca590a95ac91be53d25320fbcf2cbd3effcbb960
Rasmus Winter <rasmus@outlandish.com>
2018-04-16 12:11:04 +0100

    Replacing deprecated create_function calls

--- oowp.php
+++ oowp.php
@@ -208,7 +208,9 @@ function oofp($data, $title = null)
 function from_camel_case($str)
 {
 	$str[0] = strtolower($str[0]);
-	$func   = create_function('$c', 'return "_" . strtolower($c[1]);');
+	$func = function($c) {
+	    return '_' . strtolower($c[1]);
+	};
 	return preg_replace_callback('/([A-Z])/', $func, $str);
 }
 
@@ -223,7 +225,9 @@ function to_camel_case($str, $capitalise_first_char = false)
 	if ($capitalise_first_char) {
 		$str[0] = strtoupper($str[0]);
 	}
-	$func = create_function('$c', 'return strtoupper($c[1]);');
+	$func = function($c) {
+	    return strtoupper($c[1]);
+    };
 	return preg_replace_callback('/_([a-z])/', $func, $str);
 }
 
