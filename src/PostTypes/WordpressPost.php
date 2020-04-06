749a4188d7d651da8f2c27514a489c9deae71a37
Joaquim de Souza <joaquimds@gmail.com>
2020-04-06 11:35:00 +0100

    fix indentation

--- src/PostTypes/WordpressPost.php
+++ src/PostTypes/WordpressPost.php
@@ -623,33 +623,33 @@ abstract class WordpressPost
 		return $names;
 	}
 
-	/**
-	 * Executes a wordpress function, setting $this as the global $post first, then resets the global post data.
-	 * Expects the first argument to be the function, followed by any arguments
-	 * @return mixed
-	 */
-	protected function callGlobalPost()
-	{
+    /**
+     * Executes a wordpress function, setting $this as the global $post first, then resets the global post data.
+     * Expects the first argument to be the function, followed by any arguments
+     * @return mixed
+     */
+    protected function callGlobalPost()
+    {
         global $post;
         $prevPost = $post;
 
         // Get requested WordPress function and arguments
         $args = func_get_args();
-		$callback = array_shift($args);
+        $callback = array_shift($args);
 
-		// Set up global variables to support WP function execution
-		$post = $this->get_post();
-		setup_postdata($post);
+        // Set up global variables to support WP function execution
+        $post = $this->get_post();
+        setup_postdata($post);
 
-		// Call the WordPress function
-		$returnVal = call_user_func_array($callback, $args);
+        // Call the WordPress function
+        $returnVal = call_user_func_array($callback, $args);
 
-		// Restore original global variables
-		$post = $prevPost;
-		wp_reset_postdata();
+        // Restore original global variables
+        $post = $prevPost;
+        wp_reset_postdata();
 
-		return $returnVal;
-	}
+        return $returnVal;
+    }
 
 	public function wp_author()
 	{
