1d371b91d81f656808a588d023c29a191821aef6
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2023-11-30 14:06:05 +0200

    Make return types of functions compatible with parents
    related to the PHP 8.0+ changes in method signature compatibility when implementing interfaces or extending classes. Children classes that overwrite parent classes must have the same return type

--- src/OowpQuery.php
+++ src/OowpQuery.php
@@ -45,27 +45,27 @@ class OowpQuery extends \WP_Query implements \IteratorAggregate, \ArrayAccess, \
         return new \ArrayIterator($this->posts);
     }
 
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->posts[$offset]);
     }
 
-    public function offsetGet($offset)
+    public function offsetGet($offset): bool
     {
         return $this->posts[$offset];
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         $this->posts[$offset] = $value;
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->posts[$offset]);
     }
 
-    public function count()
+    public function count(): int
     {
         return count($this->posts);
     }
